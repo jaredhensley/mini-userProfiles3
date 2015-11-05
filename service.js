c2ee6d29b456eb2fda718340c69e2f926634d879
jaredhensley <jahensle85@me.com>
2015-11-05 11:59:22 -0600

    finished

--- service.js
+++ service.js
@@ -1,12 +1,20 @@
 var app = angular.module('userProfiles');
 
-app.service('mainService', function($http) {
+app.service('mainService', function ($http, $q) {
 
-  this.getUsers = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://reqres.in/api/users?page=1'
-    })
-  }
+	this.getUsers = function () {
+		var deferred = $q.defer();
+		$http({
+			method: 'GET',
+			url: 'http://reqres.in/api/users?page=1'
+		}).then(function (results) {
+			var parsedResults = results.data.data;
+			parsedResults.forEach(function (value, index) {
+				value.first_name = 'ralph';
+			});
+			deferred.resolve(parsedResults);
+		});
+		return deferred.promise;
+	}
 
 });
